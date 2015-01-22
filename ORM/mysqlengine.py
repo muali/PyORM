e70b9b37cc2a7d25e68af3a39d8b5764b3a3867c
Maxim Moskvitin <m-moskvitin92@yandex.ru>
2015-01-22 15:26:50 +0300

    MySQLEngine: dynamic class creation

--- ORM/mysqlengine.py
+++ ORM/mysqlengine.py
@@ -3,6 +3,7 @@
 __author__ = 'Moskvitin Maxim'
 
 import mysql.connector
+from ORM.utils.singleton import ExtendedSingleton
 
 
 def build_condition(condition_options):
@@ -17,42 +18,145 @@ def build_condition(condition_options):
     return condition
 
 
+class Table:
+    def __init__(self, name):
+        self.name = name
+        self.columns = []
+
+    def add_column(self, column):
+        self.columns += column
+
+
+class MySQLBase(object, metaclass=ExtendedSingleton):
+    _primary_keys = []
+    _fields = []
+    last_valid_state = {}
+
+    def __init__(self, engine, **kwargs):
+        self.engine = engine
+
+        if len(kwargs) != len(self._primary_keys):
+            raise TypeError("Expected: %d arguments, but recieved %d"
+                  % (len(self._primary_keys), len(kwargs)))
+
+        query_options = object()
+        query_options.entity = self.__name__
+        query_options.condition = kwargs
+
+        for key, value in kwargs:
+            if key not in self._primary_keys:
+                raise TypeError("Unexpected arg: %s" % key)
+
+        data = engine.get_object(query_options)
+        if data is None:
+            for field in self._fields:
+                object.__setattr__(self, field)
+            for key, value in kwargs:
+                object.__setattr__(self, key, value)
+            query_options.data = kwargs
+            query_options.type = "insert"
+            delattr(query_options, "condition")
+            engine.queries_options += query_options
+        else:
+            for key, value in data:
+                object.__setattr__(self, key, value)
+
+        for field in self._fields:
+            self.last_valid_state[field] = getattr(self, field)
+
+    def get_condition(self):
+        condition = {}
+        for key in self._primary_keys:
+            condition[key] = getattr(self, key)
+        return condition
+
+    def __setattr__(self, key, value):
+        if key in self._primary_keys:
+            raise Exception()
+            #TODO
+        if key in self._fields:
+            query_options = object()
+            query_options.type = "update"
+            query_options.entity = self.__name__
+            query_options.condition = self.get_condition()
+            self.engine.queries_options += query_options
+        object.__setattr__(self, key, value)
+
+    def __getattr__(self, item):
+        #TODO validate
+        object.__getattr__(self, item)
+
+
 class MySQLEngine(object):
+    queries_options = []
+
     def __init__(self, connection_options):
         self.connection_pool = mysql.connector.pooling.MySQLConnectionPool(**connection_options)
 
     def load_classes(self):
-        pass
+        def init_decorator(init):
+            def decorated(obj, **kwargs):
+                init(obj, self, **kwargs)
+            return decorated
+        tables = self.get_db_schema()
+        for table in tables:
+            newclass = self.create_class(table)
+            newclass.__init__ = init_decorator(newclass.__init__)
+            globals()[newclass.__name__] = newclass
 
-    def do_select(self, query_options):
+    def get_db_schema(self):
+        connection = self.connection_pool.get_connection()
+        cursor = connection.cursor()
+        cursor.execute("show tables")
+        tables = []
+        for (name,) in cursor:
+            tables += Table(name)
+        cursor.close()
+        cursor = connection.cursor(dictionary=True)
+        for table in tables:
+            cursor.execute("desc %s", table)
+            for column in cursor:
+                table.add_column(column)
+        return tables
+
+    @staticmethod
+    def create_class(table):
+        return type(table.name, MySQLBase, {
+            "_primary_keys": [column.Field for column in table.columns if column.Key == "PRI"],
+            "_fields": [column.Field for column in table.columns]
+        })
+
+    def get_object(self, query_options):
         connection = self.connection_pool.get_connection()
         query = ("select * from %s\n", query_options.entity)
         query += build_condition(query_options.condition)
         cursor = connection.cursor(dictionary=True)
         cursor.execute(query)
-        result = None
-        for row in cursor:
-            result = row
-            break
+        result = cursor.fetchone()
         cursor.close()
         connection.close()
         return result
 
-    def do_queries(self, queries_options):
-        connection = self.connection_pool.get_connection()
+    def do_queries(self):
         try:
-            for query_options in queries_options:
-                self.do_query(query_options, connection)
-            connection.commit()
+            connection = self.connection_pool.get_connection()
+            try:
+                for query_options in self.queries_options:
+                    self.do_query(query_options, connection)
+                connection.commit()
+            except:
+                connection.rollback()
+                raise
+            connection.close()
         except:
-            connection.rollback()
+            self.queries_options = []
             raise
-        connection.close()
 
     @staticmethod
     def do_query(query_options, connection):
+        query = ""
         if query_options.type == "insert":
-            query = ("insert into %s\n", query_options.entity)
+            query += ("insert into %s\n", query_options.entity)
             fields = "("
             values = "values ("
             is_first = True
@@ -65,11 +169,9 @@ class MySQLEngine(object):
                 fields += key
                 values += str(query_options.data[key])
             query += fields + ")\n" + values + ')'
-            cursor = connection.cursor()
-            cursor.execute(query)
 
         if query_options.type == "update":
-            query = ("update %s\n", query_options.entity)
+            query += ("update %s\n", query_options.entity)
             query += "set "
             is_first = True
             for key in query_options.data:
@@ -79,8 +181,6 @@ class MySQLEngine(object):
                     query += ','
                 query += ("%s = %s", key, str(query_options.data))
             query += '\n' + build_condition(query_options.condition)
-            cursor = connection.cursor()
-            cursor.execute(query)
-
-
-
+        cursor = connection.cursor()
+        cursor.execute(query)
+        cursor.close()
